f388bfe976f728c4a486895db8449c383a4530e8
ngs00 <ngs0726@gmail.com>
2025-08-07 15:05:04 +0900

    Update train_ssin.py

--- train_ssin.py
+++ train_ssin.py
@@ -2,11 +2,10 @@ from torch.utils.data import DataLoader
 from torch.nn.functional import sigmoid
 from sklearn.metrics import f1_score
 from util.chem import *
-from util.data import load_dataset, collate, load_ref_dataset
+from util.data import load_dataset, collate
 from method.model import SSIN
 
 
-# Set training configurations.
 random_seed = 0
 num_folds = 5
 batch_size = 128
@@ -19,24 +18,21 @@ for target_fg in func_groups.keys():
     print('------------------------ {} ------------------------'.format(target_fg))
 
     # Load the IR spectrum dataset.
-    # Activate if you need to load a new IR spectrum dataset.
     # dataset = load_dataset(path_metadata='../../data/chem_data/ir/nist/metadata.xlsx',
     #                        path_jdx='../../data/chem_data/ir/nist/jdx',
     #                        idx_smiles=4,
     #                        target_substruct=func_groups[target_fg])
     # torch.save(dataset, 'save/dataset/dataset_{}.pt'.format(target_fg))
-    dataset = torch.load('save/dataset/dataset_{}.pt'.format(target_fg))
 
+    # Load the IR spectrum dataset from a PyTorch-formatted dataset file.
+    dataset = torch.load('save/dataset/dataset_{}.pt'.format(target_fg), weights_only=False)
 
     # Load the reference dataset.
-    dataset_ref = load_ref_dataset(path_metadata='res/metadata_func_group_ref.xlsx',
-                                   path_jdx='../../data/chem_data/ir/nist/jdx',
-                                   target_substruct=target_fg)
+    dataset_ref = torch.load('res/ref_dataset.pt', weights_only=False)
 
     # Generate sub-datasets for performing k-fold cross validation.
     k_folds = dataset.get_k_folds(num_folds, random_seed=random_seed)
     list_f1 = list()
-
 
     # Train and evaluate SSIN based on the k-fold cross validation.
     for k in range(0, num_folds):
@@ -56,9 +52,14 @@ for target_fg in func_groups.keys():
         # Optimize model parameters of SSIN.
         for epoch in range(0, num_epochs):
             loss_train = model.fit(loader_train, optimizer, loss_func)
-            if (epoch + 1) % 100 == 0:
+            if (epoch + 1) % 50 == 0:
                 print('Fold [{}/{}]\tEpoch [{}/{}]\tTraining loss: {:.3f}'
                       .format(k + 1, num_folds, epoch + 1, num_epochs, loss_train))
+
+                pred_test, attns = model.predict(loader_test)
+                pred_test = sigmoid(pred_test).numpy().flatten()
+                pred_test[pred_test < 0.5] = 0
+                pred_test[pred_test >= 0.5] = 1
 
         # Execute the trained SSIN on the test dataset.
         pred_test, attns = model.predict(loader_test)
